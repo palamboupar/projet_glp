be0176246b24db0a4cad05eb8d6c24deab7aa446
palamboupar <amael.paleologos@gmail.com>
2025-01-29 15:09:33 +0100

    V1 classes a reprendre

---
+++ V1/equipmentSystem/Equipment.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package equipmentSystem;
+
+/**
+ * abstract class for the gestion of the equipment
+ */
+public abstract class Equipment {
+
+}
---
+++ V1/playerSystem/Player.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package playerSystem;
+
+import java.util.ArrayList;
+
+import equipmentSystem.*;
+
+/**
+ * abstract class for the player system
+ * started : 27/01/2025
+ * last modified : 27/01/2025
+ * @author PALEOLOGOS amael
+ * @version 0.1
+ * 
+ */
+public abstract class Player {
+/**
+ * gestion of stats for the player character
+ */
+	private double life;
+	private double lifeMAX;
+	private double mana;
+	private double manaMAX;
+	private double stamina;
+	private double staminaMAX;
+	private double xp;
+	private double nextLevel;
+	private ArrayList<Equipment> equipments;
+	
+	
+}
